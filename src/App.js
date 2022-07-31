689d9b13217ae559b36a87cf7ec6736f95a2f643
Hector Mejia <mejia7hector@gmail.com>
2022-07-30 21:57:59 -0500

    added sprite functionality

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import axios from "axios";
 
 function App() {
   const [pokemon,setPokemon] = useState(["bulbasaur,charmander"]);
-  const [currentPageUrl,setCurrentPageUrl] = useState('https://pokeapi.co/api/v2/pokemon');
+  const [currentPageUrl,setCurrentPageUrl] = useState('https://pokeapi.co/api/v2/pokemon/clefairy');
   const [prevPageUrl,setPrevPageUrl] = useState();
   const [nextPageUrl,setNextPageUrl] = useState();
   const [loading,setLoading] = useState(true);
@@ -19,7 +19,9 @@ function App() {
           setLoading(false);
           setNextPageUrl(res.data.next)
           setPrevPageUrl(res.data.previous)
-          setPokemon(res.data.results.map(p => p.name))
+          // setPokemon(res.data.map(p => p.name))
+          setPokemon(res.data.sprites.front_default)
+          console.log(res.data)
       })
 
       return () => cancel()
@@ -37,7 +39,8 @@ function gotoPrevPage() {
 
   return (
       <>
-        <PokemonList pokemon={pokemon}/>
+          <img src={pokemon} alt="icons" />
+        {/*<PokemonList pokemon={pokemon}/>*/}
         <Pagination
         gotoNextPage={nextPageUrl ? gotoNextPage : null}
         gotoPrevPage={prevPageUrl ? gotoPrevPage : null}/>
