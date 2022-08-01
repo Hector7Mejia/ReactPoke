892cc69ea5c0b1ce9538b32ef656de4961dadb35
Hector Mejia <mejia7hector@gmail.com>
2022-08-01 13:32:31 -0500

    added sprites/extras

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import axios from "axios";
 
 function App() {
   const [pokemon,setPokemon] = useState(["bulbasaur,charmander"]);
-  const [currentPageUrl,setCurrentPageUrl] = useState('https://pokeapi.co/api/v2/pokemon/clefairy');
+  const [currentPageUrl,setCurrentPageUrl] = useState('https://pokeapi.co/api/v2/pokemon/');
   const [prevPageUrl,setPrevPageUrl] = useState();
   const [nextPageUrl,setNextPageUrl] = useState();
   const [loading,setLoading] = useState(true);
@@ -16,31 +16,46 @@ function App() {
       axios.get(currentPageUrl, {
           cancelToken: new axios.CancelToken(c => cancel = c)
       }).then(res => {
-          setLoading(false);
           setNextPageUrl(res.data.next)
           setPrevPageUrl(res.data.previous)
-          // setPokemon(res.data.map(p => p.name))
-          setPokemon(res.data.sprites.front_default)
-          console.log(res.data)
+          // setPokemon(res.data.results.map(p => p.name))
+          getPokemon(res.data.results)
+          // setPokemon(res.data.map(m => m.name))
+          // setPokemon(res.data.sprites.front_default)
+          // console.log(res.data)
+          setLoading(false);
       })
 
       return () => cancel()
     },[currentPageUrl])
 
+    const getPokemon = async(res) => {
+      res.map(async(item)=> {
+          const result = await axios.get(item.url)
+          console.log(result.data)
+          setPokemon(state=>{
+              state=[...state,result.data]
+              return state;
+          })
+      })
+    }
+
 if(loading) return "loading..";
 
 function gotoNextPage() {
     setCurrentPageUrl(nextPageUrl);
+    setPokemon([])
 }
 
 function gotoPrevPage() {
     setCurrentPageUrl(prevPageUrl);
+    setPokemon([])
 }
 
   return (
       <>
-          <img src={pokemon} alt="icons" />
-        {/*<PokemonList pokemon={pokemon}/>*/}
+          {/*<img src={pokemon} alt="icons" />*/}
+        <PokemonList pokemon={pokemon}/>
         <Pagination
         gotoNextPage={nextPageUrl ? gotoNextPage : null}
         gotoPrevPage={prevPageUrl ? gotoPrevPage : null}/>
--- src/PokemonList.js
+++ src/PokemonList.js
@@ -5,8 +5,16 @@ export default function PokemonList({ pokemon }) {
     return (
         <div>
             {pokemon.map(p => (
-                <div key={p}>{p}</div>
+
+                    <div key={p.id}>
+                        <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${p.id}.svg`} alt="" />
+                        {/*<img src={p.sprites.front_default} alt="" />*/}
+                        <h2>{p.name}</h2>
+                    </div>
             ))}
         </div>
+        // <div>
+        //     {pokemon}
+        // </div>
     );
 }
